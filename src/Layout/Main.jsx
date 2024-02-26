a4354815403bbe3525796f36079117eafc8f3bce
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-26 19:46:38 +0530

    children router setup done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,8 @@
-import App from "../App";
-
+import { Outlet } from "react-router-dom";
 const Main = () => {
     return (
         <div>
-            <App></App>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import Main from "../Layout/Main";
-
+import Home from "../pages/Home/Home";
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Main></Main>
+      element: <Main></Main>,
+      children : [
+        {
+            path : "/",
+            element : <Home></Home>
+        }
+      ]
     },
   ]);
