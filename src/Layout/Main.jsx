be5b6bce4a9902850f932b50fad1e705b9ce2f8e
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-26 20:25:12 +0530

    header and footer included

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Header from "../pages/Shared/Footer/Header/Header";
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
