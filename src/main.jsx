18137beaf9ca77297fbf905e6d6fa954277d2099
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-05 20:10:27 +0530

    everything is aligned center

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </div>
 )
