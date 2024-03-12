3b3e0729d950c91fc47dfcd4fb3c4cb757e6c927
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-12 15:43:27 +0600

    button colore updated

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -19,7 +19,7 @@ const Banner = () => {
               majority have suffered alteration in some form
             </p>
             <div>
-              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-primary mr-5 bg-[#FF3811]">Discover More</button>
               <button className="btn btn-outline btn-secondary">
                 Latest Project
               </button>
@@ -36,56 +36,27 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
+        <img src={img5} className="w-full rounded-xl" />
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <div>
+            <h2 className="text-6xl text-white">Affordable Price For Car Servicing</h2>
+            <p className="text-white">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5 bg-[#FF3811] border-none"><span className="text-white">Discover More</span></button>
+              <button className="btn btn-outline btn-secondary border-white">
+                <span className="text-white">Latest Project</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle mr-3">
             ❮
           </a>
           <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={img4} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide5" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide5" className="carousel-item relative w-full">
-        <img src={img5} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide6" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide6" className="carousel-item relative w-full">
-        <img src={img6} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide5" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
             ❯
           </a>
         </div>
