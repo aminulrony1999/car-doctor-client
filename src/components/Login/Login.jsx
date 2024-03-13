ce10c5923a90a20c113113f82cbdbdaf25123c77
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-14 05:24:49 +0600

    sign in page added

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,49 @@
+import loginImg from "../../assets/images/login/login.svg";
+const Login = () => {
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-1/2 mr-12">
+            <img src={loginImg} alt="Login Image" />
+        </div>
+        <div className="card shrink-0 w-full max-w-sm">
+          <form className="card-body">
+          <h1 className="text-3xl font-bold text-center">Login</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary bg-[#FF3811] border-none rounded-lg" type="submit" value="Sign In" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
