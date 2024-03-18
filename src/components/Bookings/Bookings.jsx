02baedcaee94d5e6e1474f6ae838e1ad8649727a
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-18 16:13:47 +0600

    useState hook added

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 
 const Bookings = () => {
     const {user} = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
     const url = `http://localhost:5000/bookings?email=${user.email}`;
     useEffect(()=>{
         fetch(url)
