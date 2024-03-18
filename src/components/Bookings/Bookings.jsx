8718a7637571cf89d2c014035a1d572959d079f9
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-18 17:56:18 +0600

    booking row card created

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
+import BookingRow from "./BookingRow";
 
 const Bookings = () => {
     const {user} = useContext(AuthContext);
@@ -32,35 +33,9 @@ const Bookings = () => {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br/>
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
+      {
+        bookings.map(booking => <BookingRow key={booking._id} booking = {booking}></BookingRow>)
+      }
     </tbody>
   </table>
 </div>
