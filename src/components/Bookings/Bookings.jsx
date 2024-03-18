66cafe9403f316597ed485d156a80dc5901300a7
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-18 16:10:17 +0600

    custom url created

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../providers/AuthProviders";
 
 const Bookings = () => {
     const {user} = useContext(AuthContext);
+    const url = `http://localhost:5000/bookings?email=${user.email}`;
     return (
         <div>
             
