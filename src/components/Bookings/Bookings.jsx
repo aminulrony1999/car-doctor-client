a5c8316ea8d0978afd21f5e92fc0749f4933d9d6
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-18 16:12:12 +0600

    data loaded from mongodb

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,9 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 
 const Bookings = () => {
     const {user} = useContext(AuthContext);
     const url = `http://localhost:5000/bookings?email=${user.email}`;
+    useEffect(()=>{
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
+    },[])
     return (
         <div>
             
