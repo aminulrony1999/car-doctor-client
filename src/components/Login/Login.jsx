4bfca0ce44796012c63153a6651a62e3315b5c95
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-26 16:50:05 +0600

    token set to browser successfully

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         const user = { email };
         //get access token
         axios
-          .post("http://localhost:5000/jwt", user)
+          .post("http://localhost:5000/jwt", user,{withCredentials : true})
           .then((res) => {
             console.log(res.data);
           })
