9035e6120fe8f793406118de759625ac3ee01f46
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-26 17:05:54 +0600

    axios is used instead of fetch

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,17 +1,22 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 import BookingRow from "./BookingRow";
+import axios from "axios";
 
 const Bookings = () => {
     const {user} = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(()=>{
-        fetch(url)
-        .then(res => res.json())
-        .then(data =>{
-            setBookings(data);
+        axios.get(url)
+        .then(res => {
+          setBookings(res.data);
         })
+        // fetch(url)
+        // .then(res => res.json())
+        // .then(data =>{
+        //     setBookings(data);
+        // })
     },[url])
     const handleDelete = id =>{
       const proceed = confirm('Are you sure to delete ?');
